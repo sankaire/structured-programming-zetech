3f4aa14d854abdab5ac75ffa9559f9c9d49f1798
sankaire <kerenketepela@gmail.com>
2021-10-29 13:28:59 +0300

    CAT 1

---
+++ CAT1/main.c
@@ -0,0 +1,48 @@
+/*--------------------
+    Project: Calculator
+    Author: Tepela Kerenke
+    Licence: MIT
+    Date: fri oct 29 2021
+    compiler: GCC
+---------------------*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    //variable declaration
+    char ch;
+    double num1, num2;
+
+    //promting the user to enter numbers
+
+    printf("Enter an operator (a, s, m, d): ");
+
+    //getting characters
+    ch = getchar();
+
+    printf("Enter two intergers: ");
+
+    //storing the numbers
+    scanf("%lf %lf", &num1, &num2);
+
+    //comutation and output
+    switch(ch) {
+        case 'a':
+        printf("%.1lf + %.1lf = %.1lf \n", num1, num2, num1 + num2);
+        break;
+        case 's':
+        printf("%.1lf - %.1lf = %.1lf \n", num1, num2, num1 - num2);
+        break;
+        case 'm':
+        printf("%.1lf * %.1lf = %.1lf \n", num1, num2, num1 * num2);
+        break;
+        case 'd':
+        printf("%.1lf / %.1lf = %.1lf \n", num1, num2, num1 / num2);
+        break;
+        default:
+        printf("Invalid Input");
+    }
+
+  return 0;
+}
